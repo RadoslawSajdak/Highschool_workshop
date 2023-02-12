8cb56957d24a25efd84f53016d71386ed9991345
RadoslawSajdak <sajdak.radoslaw@gmail.com>
2023-02-12 16:20:13 +0100

    Added gpio file

--- dark_mode.js
+++ dark_mode.js
@@ -1,11 +1,5 @@
 const checkbox = document.getElementById('checkbox');
-var state = false;
 
 checkbox.addEventListener('change', ()=>{
   document.body.classList.toggle('dark');
-  state != state;
 })
-
-if (state){
-  document.body.classList.toggle('dark');
-}
