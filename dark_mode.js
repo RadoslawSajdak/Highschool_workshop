254dc5f200761a850764468009b966f59a37ad12
RadoslawSajdak <sajdak.radoslaw@gmail.com>
2023-02-12 15:11:15 +0100

    save dark mode

--- dark_mode.js
+++ dark_mode.js
@@ -1,5 +1,11 @@
 const checkbox = document.getElementById('checkbox');
+var state = false;
 
 checkbox.addEventListener('change', ()=>{
   document.body.classList.toggle('dark');
+  state != state;
 })
+
+if (state){
+  document.body.classList.toggle('dark');
+}
